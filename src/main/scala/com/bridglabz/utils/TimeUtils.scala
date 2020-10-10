f439c73f6da3572e7cb97107a8ea8307fcc39590
Ranbir7925 <honeyranbir7925@gmail.com>
2020-10-10 20:58:14 +0530

    [Ranbir]ADDED:Code to add time delay

---
+++ src/main/scala/com/bridglabz/utils/TimeUtils.scala
@@ -0,0 +1,8 @@
+package com.bridglabz.utils
+
+import scala.concurrent.duration.{DurationInt, FiniteDuration}
+
+object TimeUtils  {
+  val atMostDuration: FiniteDuration = 2.seconds
+  val timeoutMills: Long = 2 * 1000
+}
