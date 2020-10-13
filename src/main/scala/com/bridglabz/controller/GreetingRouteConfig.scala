b997cf56d6b7995c070b66ac82c50c970faba373
Ranbir7925 <honeyranbir7925@gmail.com>
2020-10-13 13:14:43 +0530

    [Ranbir]ADDED:Code for get user by id API

--- src/main/scala/com/bridglabz/controller/GreetingRouteConfig.scala
+++ src/main/scala/com/bridglabz/controller/GreetingRouteConfig.scala
@@ -14,38 +14,50 @@ import akka.stream.scaladsl.Source
 import akka.util.ByteString
 import com.bridglabz.controller._
 import com.bridglabz.models.{Greeting, GreetingRequest}
+import com.bridglabz.service.GreetingService
 import spray.json.enrichAny
 
 import scala.concurrent.Await
 
 /**
+ * Routing of all API's done here
  *
- * @param system
+ * @param system take implicit actor system type value
  */
 class GreetingRouteConfig(implicit val system: ActorSystem) extends JsonUtils {
   val greetingActor: ActorRef = system.actorOf(Props(new GreetingActor()))
+  val greetingData = new GreetingService
 
   implicit val materializer: ActorMaterializer = ActorMaterializer()
 
   lazy val getRoute: Route =
     PathDirectives.pathPrefix("api") {
-//      concat(
-        path("create") {
-          post {
-            entity(as[GreetingRequest]) { greeting =>
-              Patterns.ask(greetingActor, SAVE(greeting), TimeUtils.timeoutMills)
-              RouteDirectives.complete(HttpEntity("Data saved successfully!"))
+      path("create") {
+        post {
+          entity(as[GreetingRequest]) { greeting =>
+            Patterns.ask(greetingActor, SAVE(greeting), TimeUtils.timeoutMills)
+            RouteDirectives.complete(HttpEntity("Data saved successfully!"))
+          }
+        }
+      } ~
+        path("greeting") {
+          get {
+            parameter("id") { id =>
+              val resultFuture = Patterns.ask(greetingActor, SEARCH_Id(id), TimeUtils.timeoutMills)
+              val resultSource = Await.result(resultFuture, TimeUtils.atMostDuration).asInstanceOf[Source[Greeting, NotUsed]]
+              val resultByteString = resultSource.map { it => ByteString.apply(it.toJson.toString) }
+              RouteDirectives.complete(HttpEntity(ContentTypes.`text/plain(UTF-8)`, resultByteString))
+
             }
           }
-        }~
-        //  }
-        //                        path("search") {
-        get {
-          val resultFuture = Patterns.ask(greetingActor, SEARCH_ALL, TimeUtils.timeoutMills)
-          val resultSource = Await.result(resultFuture, TimeUtils.atMostDuration).asInstanceOf[Source[Greeting, NotUsed]]
-          val resultByteString = resultSource.map { it => ByteString.apply(it.toJson.toString.getBytes()) }
-          RouteDirectives.complete(HttpEntity(ContentTypes.`text/plain(UTF-8)`, resultByteString))
+        } ~
+        path("greetings") {
+          get {
+            val resultFuture = Patterns.ask(greetingActor, SEARCH_ALL, TimeUtils.timeoutMills)
+            val resultSource = Await.result(resultFuture, TimeUtils.atMostDuration).asInstanceOf[Source[Greeting, NotUsed]]
+            val resultByteString = resultSource.map { it => ByteString.apply(it.toJson.toString) }
+            RouteDirectives.complete(HttpEntity(ContentTypes.`text/plain(UTF-8)`, resultByteString))
+          }
         }
-//      )
     }
 }
