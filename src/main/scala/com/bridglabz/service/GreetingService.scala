122f56b4f0955605296f286f6293cbd3f4f8fbad
Ranbir7925 <honeyranbir7925@gmail.com>
2020-10-13 13:19:27 +0530

    [Ranbir]ADDED:Code to get data from repository layer of user by id

--- src/main/scala/com/bridglabz/service/GreetingService.scala
+++ src/main/scala/com/bridglabz/service/GreetingService.scala
@@ -2,8 +2,8 @@ package com.bridglabz.service
 
 import akka.NotUsed
 import akka.stream.scaladsl.Source
-import com.bridglabz.models.{Greeting, GreetingRequest}
 import com.bridglabz.Repository.GreetingRepo
+import com.bridglabz.models.{Greeting, GreetingRequest}
 import org.mongodb.scala.Completed
 
 import scala.concurrent.Await
@@ -12,14 +12,14 @@ import scala.concurrent.duration.Duration
 class GreetingService {
   /**
    * Send data to GreetingRepo which is responsible for further processing
-   * @return
    */
   def saveGreetingData: GreetingRequest => Completed = (greetingRequest: GreetingRequest) => {
-    Await.result(GreetingRepo.insertData(greetingRequest),Duration.Inf)
+    Await.result(GreetingRepo.insertData(greetingRequest), Duration.Inf)
   }
 
   /**
    * Get all data from GreetingRepo
+   *
    * @return A stream which will be processed further
    */
   def findAll: Source[Greeting, NotUsed] = {
@@ -27,5 +27,14 @@ class GreetingService {
       .mapConcat {
         identity
       }
+  }
+
+  /**
+   * Get data by id from GreetingRepo
+   *
+   * @return A stream which will be processed further
+   */
+  def findId(id: String): Source[Greeting, NotUsed] = {
+    Source.fromFuture(GreetingRepo.findID(id))
   }
 }
