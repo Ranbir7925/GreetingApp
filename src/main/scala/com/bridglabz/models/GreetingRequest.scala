2cba1bccc43d9787e82553de6f2e610a0b0a69fc
Ranbir7925 <honeyranbir7925@gmail.com>
2020-10-13 13:16:39 +0530

    [Ranbir]ADDED:Proper documenation for case classes

--- src/main/scala/com/bridglabz/models/GreetingRequest.scala
+++ src/main/scala/com/bridglabz/models/GreetingRequest.scala
@@ -2,8 +2,9 @@ package com.bridglabz.models
 
 /**
  * Class accepting Sting Value parameters
- * @param name -> In String Format
- * @param message-> In String Format
+ *
+ * @param name    -> In String Format
+ * @param message -> In String Format
  */
 case class GreetingRequest(name: String, message: String) {
   require(!name.isEmpty, "Name Can't be Empty")
